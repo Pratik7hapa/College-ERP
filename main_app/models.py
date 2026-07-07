64db6010b0f44904ef7897f3a0f52d8c45c41fea
Pratik Thapa <pratikthapa@Pratik-Mac.local>
2026-07-07 16:02:59 +0545

    2nd commit

--- main_app/models.py
+++ main_app/models.py
@@ -58,7 +58,7 @@ class CustomUser(AbstractUser):
     objects = CustomUserManager()
 
     def __str__(self):
-        return  self.first_name + " " + self.last_name
+        return f"{self.first_name} {self.last_name}"
 
 
 class Admin(models.Model):
@@ -90,7 +90,7 @@ class Student(models.Model):
     session = models.ForeignKey(Session, on_delete=models.DO_NOTHING, null=True)
 
     def __str__(self):
-        return self.admin.last_name + ", " + self.admin.first_name
+        return f"{self.admin.first_name} {self.admin.last_name}"
 
 class Library(models.Model):
     student = models.ForeignKey(Student,  on_delete=models.CASCADE, null=True, blank=False)
